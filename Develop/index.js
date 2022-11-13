918c93932323e48739a76c3412a9ca76f0787fc2
William Huang <hh921088586@berkeley.edu>
2022-11-12 16:11:02 -0800

    small changes on index.js function

--- Develop/index.js
+++ Develop/index.js
@@ -51,15 +51,17 @@ function startQuestions() {
             'Intern',
             'No'
         ]
-    }]).then(function(date) {
-        console.log(data);
-        addEmployee = true;
+    }]).then(function(data) {
+      let manager = new Manager(data.nameEmployee, data.idEmployee, data.emailEmployee, data.managerNumber);
+      employees.push(manager);
+        // console.log(data);
+        // addEmployee = true;
         switch (data.another) {
             case 'Engineer':
                 engineerQuestions();
                 break;
             case 'Intern':
-                internQuestions()
+                internQuestions();
         }
     })
 }
@@ -81,7 +83,6 @@ function engineerQuestions() {
       type: "input",
       name: "gitusernameEngineer",
       message: "Enter the employee's github username: ",
-      // when: (answers) => answers.role === 'Engineer'
     }, {
       type: "list",
       name: "another",
@@ -92,7 +93,9 @@ function engineerQuestions() {
         "No"
       ]
     }]).then(function (data) {
-      console.log("you entered engineer questions");
+      let engineer = new Engineer(data.nameEmployee, data.idEmployee, data.emailEmployee, data.gitusernameEngineer)
+      employees.push(engineer);
+      // console.log("you entered engineer questions");
       switch (data.another) {
         case "Engineer":
           engineerQuestions();
@@ -108,35 +111,43 @@ function engineerQuestions() {
     inquirer.prompt([{
         type: "input",
         name: "nameEmployee",
-        message: "Enter employee's name:"
+        message: "Enter the employee's name: "
       }, {
         type: "input",
         name: "idEmployee",
-        message: "Enter employee ID number: "
+        message: "Enter the employee's ID number: "
       }, {
         type: "input",
         name: "emailEmployee",
-        message: "Enter employee email address: "
+        message: "Enter the employee's email address: "
       }, {
         type: "input",
         name: "schoolIntern",
-        message: "Enter school intern attends: ",
-        // when: (answers) => answers.role === 'Intern'
+        message: "Enter attened school: ",
       }, {
-        type: "confirm",
-        name: "another",
-        message: "Enter another team member?",
-        choices: ['Yes', 'No']
+        type: "list",
+      name: "another",
+      message: "Would you like to add another member?",
+      choices: [
+        "Engineer",
+        "Intern",
+        "No"
+      ]
       }])
       .then(function (data) {
-        if (data.choices === 'Yes') {
-          startQuestions();
-        } else {
-          renderHtml();
+        let intern = new Intern(data.nameEmployee, data.idEmployee, data.emailEmployee, data.schoolIntern)
+      employees.push(intern);
+        switch (data.another) {
+          case 'Engineer':
+            engineerQuestions();
+            case 'Intern':
+              internQuestions();
         }
   
       })
   }
+
+  
 
     // function startQuestions() {
     //     inquirer.prompt([
